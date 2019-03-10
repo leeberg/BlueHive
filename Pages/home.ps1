c88c1f310ff2596f1fa7c56dac426d8f793e699d
Lee Berg <leealanberg@gmail.com>
2019-03-09 23:33:10 -0600

    Simpel Metrics

--- Pages/home.ps1
+++ Pages/home.ps1
@@ -1,17 +1,42 @@
 
 
 New-UDPage -Name "Home" -Icon home -Content {
-            
-       
-    $JsonData = Get-BHHoneyAccountData
-    New-UDGrid -Title "Managed Honey Account Users" -Headers @("Name","Enabled","DeploymentDate") -Properties @("DisplayName", "Enabled", "whenCreated") -Endpoint {    
-            $JsonData | Out-UDGridData
+
+    $HoneyUsers = Get-BHHoneyAccountData
+    $UserAccounts = Get-BHAccountData
+    $History = Get-BHDeploymentHistoryData
+
+    New-UDRow -Columns {
+        
+
+        New-UDColumn -Size 2 {    
+
+            New-UDCounter -Title "Deployed Honey Accounts" -Endpoint {
+                ($HoneyUsers | Measure).Count | ConvertTo-Json
+            } -FontColor "black"
+
+        }
+
+        New-UDColumn -Size 2 {    
+
+                New-UDCounter -Title "Deployment Actions" -Endpoint {
+                    ($History | Measure).Count | ConvertTo-Json
+                } -FontColor "black"
+    
+            }
+
     }
 
 
-    $JsonData = Get-BHAccountData
+
+    New-UDGrid -Title "Managed Honey Account Users" -Headers @("Name","Enabled","DeploymentDate") -Properties @("DisplayName", "Enabled", "whenCreated") -Endpoint {    
+            $HoneyUsers | Out-UDGridData
+    }
+
+
+    
     New-UDGrid -Title "All Active Directory Users" -Headers @("Name", "GivenName", "Surname","Enabled") -Properties @("Name", "GivenName", "Surname","Enabled") -Endpoint {    
-            $JsonData | Out-UDGridData
+            $UserAccounts | Out-UDGridData
     }
     
     
